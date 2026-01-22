0dfd34319230a58cb74e14bb7f9e4a829cf64f25
33bca <fabian.leutenegger@bluewin.ch>
2026-01-22 08:48:32 +0000

    Link dolby blobs against v33 libstagefright_foundation
    * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib8852f8aa5d5c6625d1f7e01348c7fdcdb36e414
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/libdlbpreg.so',
+        'vendor/lib/soundfx/libswdap.so',
+        'vendor/lib/soundfx/libdlbvol.so',
+        'vendor/lib64/libcodec2_soft_ac4dec.so',
+        'vendor/lib64/libcodec2_soft_ddpdec.so',
+        'vendor/lib64/libcodec2_soft_dolby.so',
+        'vendor/lib64/libdlbdsservice.so',
+        'vendor/lib64/libdlbpreg.so',
+        'vendor/lib64/soundfx/libdlbvol.so',
+        'vendor/lib64/soundfx/libswdap.so'
+    ): blob_fixup().replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'dolby',
     'sony',
+    blob_fixups=blob_fixups,
     device_rel_path='vendor/sony/dolby',
 )
 
